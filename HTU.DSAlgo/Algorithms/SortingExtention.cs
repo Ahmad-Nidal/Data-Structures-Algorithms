5ac60d76c0f8e26150a4d27c7df58c5ae6fa13f2
Ahmad Nidal <ahmadnidal.dr@gmil.com>
2023-05-23 09:56:09 +0300

    merge sort

--- HTU.DSAlgo/Algorithms/SortingExtention.cs
+++ HTU.DSAlgo/Algorithms/SortingExtention.cs
@@ -25,5 +25,67 @@ namespace HTU.DSAlgo.Algorithms
                 list[i] = min;
             }
         }
+
+        public static int[] MergeSort(this int[] arr)
+        {
+            int n = arr.Length;
+            if (n < 2) return arr;
+
+            int mid = n / 2;
+            var leftArr = new int[mid];
+            var rightArr = new int[n - mid];
+
+            // Fill left array
+            for (int i = 0; i < mid; i++)
+            {
+                leftArr[i] = arr[i];
+            }
+
+            // Fill right array
+            for (int i = mid; i < n; i++)
+            {
+                rightArr[i - mid] = arr[i];
+            }
+
+            leftArr = leftArr.MergeSort();
+            rightArr = rightArr.MergeSort();
+
+            // Merge left and right arrays
+            return leftArr.Merge(rightArr);
+        }
+
+        private static int[] Merge(this int[] leftArr, int[] rightArr)
+        {
+            int leftLen = leftArr.Length;
+            int rightLen = rightArr.Length;
+            int[] mergedArr = new int[leftLen + rightLen];
+            int i = 0, j = 0, k = 0;
+
+            // Merge with sorting
+            while (i < leftLen && j < rightLen)
+            {
+                if (leftArr[i] <= rightArr[j])
+                {
+                    mergedArr[k++] = leftArr[i++];
+                }
+                else
+                {
+                    mergedArr[k++] = rightArr[j++];
+                }
+            }
+
+            // Copy remaining elements
+            while (i < leftLen)
+            {
+                mergedArr[k++] = leftArr[i++];
+            }
+
+            while (j < rightLen)
+            {
+                mergedArr[k++] = rightArr[j++];
+            }
+
+            return mergedArr;
+        }
     }
 }
--- HTU.DSAlgo/Program.cs
+++ HTU.DSAlgo/Program.cs
@@ -6,16 +6,19 @@ namespace HTU.DSAlgo
     {
         public static void Main(string[] args)
         {
+            var arr = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
 
-            var list = new List<int> { 4, 5, 1, 3, 2, 7, 8, 9 };
-            var list2 = new List<int> { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            arr = arr.MergeSort();
 
-            list.SelectionSort();
-            list2.SelectionSort();
-
-            list.ForEach(x => Console.WriteLine(x));
             Console.WriteLine("--------------");
-            list2.ForEach(x => Console.WriteLine(x));
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
